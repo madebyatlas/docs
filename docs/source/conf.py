d2386c2387d37708f567a692c2e5c4d219423745
Atlas Development <atlasdevelopmentus@outlook.com>
2024-11-02 00:07:20 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Atlas Development'
+copyright = '2024'
+author = 'McGRiM'
 
 release = '0.1'
 version = '0.1.0'
